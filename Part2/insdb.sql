dbc2ac96bfca104dee5e21af5d0c040ed3601aab
layth624 <68213233+layth624@users.noreply.github.com>
2023-02-21 22:28:09 -0700

    Update insdb.sql

--- Part2/insdb.sql
+++ Part2/insdb.sql
@@ -32,3 +32,38 @@ INSERT INTO Client(clientId,name,phone,address) VALUES
 (3,"Jane Doe","800-555-5555","4009 31st St."),
 (4,"Fistname Lastname","800-555-5555","2408 E FM 1729"),
 (5,"DROP TABLE Client;","800-555-5555","Space");
+
+INSERT INTO TechnicalSupport(empId,name,gender) VALUES
+(123,"Happy Marcus","F"),
+(229,"Lucas Frost","M"),
+(365,"Basin Malory","M"),
+(487,"Nigma Edwards","F"),
+(501,"Destiny Mallot","M");
+
+INSERT INTO Administrator(empId,name,gender) VALUES
+(1,"Andebert Manjeet","M"),
+(2,"Eula Bert","F"),
+(3,"Javion Bernhard","M"),
+(4,"Ella Demelza","F"),
+(5,"Spencer Archibald","M");
+
+INSERT INTO Salesman(empId,name,gender) VALUES
+(1,"Cheyanne Deforrest","F"),
+(2,"Odin Indy","M"),
+(3,"Sly Bloxham","M"),
+(4,"Kristina Arden","F"),
+(5,"Marylu Diamond","F");
+
+INSERT INTO AirtimePackage(packageId,class,startDate,lastDate,frequency,videoCode) VALUES
+(1,"economy","2020-06-15","2020-07-15",4,11),
+(2,"golden hours","2020-09-20","2020-10-20",5,21),
+(3,"whole day","2020-11-30","2021-01-22",3,31),
+(4,"economy","2021-03-13","2021-04-15",4,41),
+(5,"golden hours","2023-01-23","2023-02-19",6,51);
+
+INSERT INTO AdmWorkHours(empId,day,hours) VALUES
+(1,"2020-06-15",08.12),
+(2,"2020-06-15",07.53),
+(3,"2020-06-15",05.75),
+(4,"2020-06-15",08.34),
+(5,"2020-06-15",07.94);
